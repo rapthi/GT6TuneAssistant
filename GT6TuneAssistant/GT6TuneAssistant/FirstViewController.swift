b22c880ed75f197ad4a84cb9a9e051eda9bfa061
Thierry Rapillard <trapillard@quantesys.com>
2014-10-15 09:22:03 +0200

    Create new xcode project with cocoapods included

---
+++ GT6TuneAssistant/GT6TuneAssistant/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GT6TuneAssistant
+//
+//  Created by Thierry Rapillard on 15/10/14.
+//  Copyright (c) 2014 Quantesys. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
